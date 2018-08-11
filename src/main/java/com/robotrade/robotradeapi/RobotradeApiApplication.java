232222f49d5d19b99fe12690323f0ec5d8c39857
peter palocz <peter.palocz91@gmail.com>
2018-08-11 17:40:59 +0200

    initial commit

---
+++ src/main/java/com/robotrade/robotradeapi/RobotradeApiApplication.java
@@ -0,0 +1,12 @@
+package com.robotrade.robotradeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotradeApiApplication.class, args);
+	}
+}
