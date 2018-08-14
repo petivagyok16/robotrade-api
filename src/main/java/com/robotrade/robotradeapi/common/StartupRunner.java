415a9a0d9c3d4afebe592072201d8042abcf420e
peter palocz <peter.palocz91@gmail.com>
2018-08-14 21:46:32 +0200

    feat(StartupRunner): add startup runner

---
+++ src/main/java/com/robotrade/robotradeapi/common/StartupRunner.java
@@ -0,0 +1,37 @@
+package com.robotrade.robotradeapi.common;
+
+import com.robotrade.robotradeapi.models.User;
+import com.robotrade.robotradeapi.repository.UserRepository;
+import com.robotrade.robotradeapi.security.CustomPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("localDB")
+public class StartupRunner implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final CustomPasswordEncoder passwordEncoder;
+
+	public StartupRunner(
+					UserRepository userRepository,
+					CustomPasswordEncoder passwordEncoder
+	) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(" ------- Loading dummy data into MongoDB ------- ");
+		this.addUser();
+	}
+
+	private void addUser() {
+		User testUser = new User("1", "test", this.passwordEncoder.encode("test"), 5.00, 10.00);
+		this.userRepository.save(testUser);
+	}
+}
