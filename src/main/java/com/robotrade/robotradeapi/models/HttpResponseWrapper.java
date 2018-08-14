c656f758160407b64d9a101ebbc01c9dffc2841f
peter palocz <peter.palocz91@gmail.com>
2018-08-14 22:15:54 +0200

    feat(ResponseWrappers): add wrappers

---
+++ src/main/java/com/robotrade/robotradeapi/models/HttpResponseWrapper.java
@@ -0,0 +1,17 @@
+package com.robotrade.robotradeapi.models;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class HttpResponseWrapper<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private T payload;
+
+	public HttpResponseWrapper(T payload) {
+		this.payload = payload;
+	}
+}
+
---
+++ src/main/java/com/robotrade/robotradeapi/models/LoginResponseWrapper.java
@@ -0,0 +1,18 @@
+package com.robotrade.robotradeapi.models;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class LoginResponseWrapper<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private T payload;
+	private String token;
+
+	public LoginResponseWrapper(T payload, String token) {
+		this.payload = payload;
+		this.token = token;
+	}
+}
