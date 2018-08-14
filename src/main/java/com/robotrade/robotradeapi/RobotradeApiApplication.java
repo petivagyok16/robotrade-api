9aa46b46ebbfe3fdce6caca8c4bb54bca4fe1f0e
peter palocz <peter.palocz91@gmail.com>
2018-08-14 21:46:18 +0200

    feat(App): add app type

--- src/main/java/com/robotrade/robotradeapi/RobotradeApiApplication.java
+++ src/main/java/com/robotrade/robotradeapi/RobotradeApiApplication.java
@@ -1,12 +1,15 @@
 package com.robotrade.robotradeapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RobotradeApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RobotradeApiApplication.class, args);
+		SpringApplication app = new SpringApplication(RobotradeApiApplication.class);
+		app.setWebApplicationType(WebApplicationType.REACTIVE);
+		app.run(args);
 	}
 }
