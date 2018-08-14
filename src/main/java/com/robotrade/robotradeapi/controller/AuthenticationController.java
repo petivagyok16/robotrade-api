1d67787b372ded33e2d4159663e0ea6c1220c2a4
peter palocz <peter.palocz91@gmail.com>
2018-08-14 22:16:07 +0200

    feat(Controllers): add controllers

---
+++ src/main/java/com/robotrade/robotradeapi/controller/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.robotrade.robotradeapi.controller;
+
+import com.robotrade.robotradeapi.models.HttpResponseWrapper;
+import com.robotrade.robotradeapi.models.LoginResponseWrapper;
+import com.robotrade.robotradeapi.models.User;
+import com.robotrade.robotradeapi.security.JwtAuthenticationRequest;
+import com.robotrade.robotradeapi.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@Validated
+@CrossOrigin(origins = "*")
+public class AuthenticationController {
+
+	private final AuthenticationService authService;
+
+	@Autowired
+	public AuthenticationController(AuthenticationService authService) {
+		this.authService = authService;
+	}
+
+	@PostMapping(
+					value = "/signin",
+					consumes = MediaType.APPLICATION_JSON_VALUE,
+					produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public Mono<ResponseEntity<LoginResponseWrapper<User>>> signIn(@Valid @RequestBody JwtAuthenticationRequest authenticationRequest) {
+		return this.authService.signIn(authenticationRequest);
+	}
+
+	@PostMapping(
+					value = "/signup",
+					consumes =  MediaType.APPLICATION_JSON_VALUE
+	)
+	public Mono<ResponseEntity> signUp(@Valid @RequestBody User user) {
+		return this.authService.signUp(user);
+	}
+
+	@GetMapping(
+					value = "/me",
+					produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public Mono<ResponseEntity<HttpResponseWrapper<User>>> getAuthenticatedUser(@RequestHeader(value = "Authorization") String bearerToken) {
+		return this.authService.getAuthenticatedUser(bearerToken);
+	}
+}
---
+++ src/main/java/com/robotrade/robotradeapi/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.robotrade.robotradeapi.controller;
+
+import com.robotrade.robotradeapi.models.HttpResponseWrapper;
+import com.robotrade.robotradeapi.models.User;
+import com.robotrade.robotradeapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@RestController
+@RequestMapping(path = "/api/v1/users", produces = { APPLICATION_JSON_UTF8_VALUE })
+@CrossOrigin(origins = "*")
+@Validated
+public class UserController {
+
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public Mono<ResponseEntity<HttpResponseWrapper<List<User>>>> getAllUser() {
+		return this.userService.getAllUser();
+	}
+
+	@GetMapping(value = "/{id}")
+	public Mono<ResponseEntity<HttpResponseWrapper<User>>> getUserProfile(@NotNull @PathVariable("id") String id) {
+		return this.userService.getUserProfile(id);
+	}
+}
