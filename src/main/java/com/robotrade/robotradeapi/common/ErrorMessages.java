0526ce18078bb5b98694327bea68062889c463de
peter palocz <peter.palocz91@gmail.com>
2018-08-14 21:34:42 +0200

    feat(ErrorMessages): add error messages

---
+++ src/main/java/com/robotrade/robotradeapi/common/ErrorMessages.java
@@ -0,0 +1,11 @@
+package com.robotrade.robotradeapi.common;
+
+public class ErrorMessages {
+	public static final String USER_NOT_FOUND = "User not found!";
+	public static final String USER_ALREADY_EXISTS = "User already exists";
+	public static final String WRONG_CREDENTIALS = "Wrong username or password!";
+	public static final String INVALID_TOKEN = "Invalid authentication token!";
+	public static final String EDIT_USER_NOT_ALLOWED = "Editing the user is not allowed!";
+	public static final String DATABASE_ERROR = "Something went wrong during the process. Please try again later!";
+	public static final String SOURCE_NOT_FOUND = "One of the requested resource is not found.";
+}
