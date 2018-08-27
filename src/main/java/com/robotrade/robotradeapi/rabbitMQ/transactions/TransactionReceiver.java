0a41b162db75e823357d6bbf71f59b685519a7eb
peter palocz <peter.palocz91@gmail.com>
2018-08-27 22:30:09 +0200

    feat(TransactionReceiver): distribute transactions

--- src/main/java/com/robotrade/robotradeapi/rabbitMQ/transactions/TransactionReceiver.java
+++ src/main/java/com/robotrade/robotradeapi/rabbitMQ/transactions/TransactionReceiver.java
@@ -3,11 +3,24 @@ package com.robotrade.robotradeapi.rabbitMQ.transactions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.robotrade.robotradeapi.rabbitMQ.constants.TransactionConstants;
 import com.robotrade.robotradeapi.rabbitMQ.models.Transaction;
+import com.robotrade.robotradeapi.service.TransactionsCalculationService;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 @Slf4j
+@NoArgsConstructor
 public class TransactionReceiver {
+
+	@Autowired
+	private TransactionsCalculationService transactionsCalculationService;
+
+	public TransactionReceiver(TransactionsCalculationService transactionsCalculationService) {
+		this.transactionsCalculationService = transactionsCalculationService;
+	}
 
 	@RabbitListener(queues = TransactionConstants.ROBO_TRANSACTION_QUEUE_NAME)
 	public void receiveTransaction(String transaction) {
@@ -21,6 +34,9 @@ public class TransactionReceiver {
 			Transaction transaction = mapper.readValue(transactionString, Transaction.class);
 			log.info(" ****** TRANSACTION RECEIVED: ***** ");
 			log.info(transaction.toString());
+			transaction.setId(UUID.randomUUID().toString());
+			this.transactionsCalculationService.distributeTransaction(transaction);
+
 		} catch (Exception e) {
 			log.info(" --- Error during processing JSON value from Rabbit --- ");
 			e.printStackTrace();
