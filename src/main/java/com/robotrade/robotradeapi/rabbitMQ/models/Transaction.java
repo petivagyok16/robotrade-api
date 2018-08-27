162e7d6d094af8e907362415c586716a47cb02f1
peter palocz <peter.palocz91@gmail.com>
2018-08-27 22:28:45 +0200

    feat(Transaction): add id and date

--- src/main/java/com/robotrade/robotradeapi/rabbitMQ/models/Transaction.java
+++ src/main/java/com/robotrade/robotradeapi/rabbitMQ/models/Transaction.java
@@ -4,13 +4,17 @@ import lombok.Data;
 
 @Data
 public class Transaction {
+
+	private String id;
 	private Number type;
 	private Double cash;
 	private Double stock;
+	private Number date;
 
-	public Transaction(Number type, Double cash, Double stock) {
+	public Transaction(Number type, Double cash, Double stock, Number date) {
 		this.type = type;
 		this.cash = cash;
 		this.stock = stock;
+		this.date = date;
 	}
 }
