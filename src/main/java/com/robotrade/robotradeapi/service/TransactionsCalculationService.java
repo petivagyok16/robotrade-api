3a0486672274d30ea1bedbc4dc42b2c69203f946
peter palocz <peter.palocz91@gmail.com>
2018-08-27 22:29:29 +0200

    feat(TransactionsCalculator): add service

--- src/main/java/com/robotrade/robotradeapi/service/TransactionsCalculationService.java
+++ src/main/java/com/robotrade/robotradeapi/service/TransactionsCalculationService.java
@@ -2,12 +2,19 @@ package com.robotrade.robotradeapi.service;
 
 import com.robotrade.robotradeapi.models.User;
 import com.robotrade.robotradeapi.rabbitMQ.models.AllUsersCapital;
+import com.robotrade.robotradeapi.rabbitMQ.models.Transaction;
 import com.robotrade.robotradeapi.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class TransactionsCalculationService {
 	private final UserRepository userRepository;
 
@@ -21,5 +28,24 @@ public class TransactionsCalculationService {
 						.map(User::getCash)
 						.reduce(0.00, (c1, c2) -> c1 + c2)
 						.flatMap(capital -> Mono.just(new AllUsersCapital(capital)));
+	}
+
+	// This is just a mock transaction history loader atm
+	public void distributeTransaction(Transaction transaction) {
+		this.userRepository
+						.findAll()
+						.collectList()
+						.publishOn(Schedulers.parallel())
+						.flatMap(users -> Mono.just(users.stream()
+										.map(user -> {
+											List<Transaction> existingTransactions = user.getTransactionHistory();
+											existingTransactions.add(transaction);
+											user.setTransactionHistory(existingTransactions);
+											return user;
+										})
+										.collect(Collectors.toList()))
+						)
+						.flatMap(users -> this.userRepository.saveAll(users).collectList())
+						.subscribe(users -> log.info("Modified users saved!"));
 	}
 }
