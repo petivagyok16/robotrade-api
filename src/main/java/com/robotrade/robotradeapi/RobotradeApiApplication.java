d509d1ec42b513874c30ffc3a1a44267d876dbb3
peter palocz <peter.palocz91@gmail.com>
2018-08-31 14:47:49 +0200

    fix(App): fix db connection

--- src/main/java/com/robotrade/robotradeapi/RobotradeApiApplication.java
+++ src/main/java/com/robotrade/robotradeapi/RobotradeApiApplication.java
@@ -3,10 +3,8 @@ package com.robotrade.robotradeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class RobotradeApiApplication {
 
 	public static void main(String[] args) {
