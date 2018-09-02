4d047c2359aa9f67697b60e6e3988e3dbcb14717
peter palocz <peter.palocz91@gmail.com>
2018-09-02 19:39:05 +0200

    feat(CloudAMQPConfig): add rabbit listener container

--- src/main/java/com/robotrade/robotradeapi/rabbitMQ/config/CloudAMQPConfig.java
+++ src/main/java/com/robotrade/robotradeapi/rabbitMQ/config/CloudAMQPConfig.java
@@ -1,6 +1,8 @@
 package com.robotrade.robotradeapi.rabbitMQ.config;
 
 import com.robotrade.robotradeapi.rabbitMQ.constants.CloudAMPQCredentials;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class CloudAMQPConfig {
 
 	private final CachingConnectionFactory cloudAMPQConnection;
@@ -23,6 +26,14 @@ public class CloudAMQPConfig {
 		connectionFactory.setConnectionTimeout(30000);
 
 		this.cloudAMPQConnection = connectionFactory;
+	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConcurrentConsumers(2);
+		factory.setConnectionFactory(this.cloudAMPQConnection);
+		return factory;
 	}
 
 	@Bean
