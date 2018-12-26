d92ebdd8f0f06afce2b07ec878758623a46be5d4
peter palocz <peter.palocz91@gmail.com>
2018-12-26 16:42:39 +0100

    feat: add transaction calculation

--- src/main/java/com/robotrade/robotradeapi/rabbitMQ/dev/transactions/TransactionReceiver.java
+++ src/main/java/com/robotrade/robotradeapi/rabbitMQ/dev/transactions/TransactionReceiver.java
@@ -3,13 +3,22 @@ package com.robotrade.robotradeapi.rabbitMQ.dev.transactions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.robotrade.robotradeapi.models.TraderBotTransaction;
 import com.robotrade.robotradeapi.rabbitMQ.constants.TransactionConstants;
+import com.robotrade.robotradeapi.service.TransactionsCalculationService;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 @NoArgsConstructor
 public class TransactionReceiver {
+
+	@Autowired
+	private TransactionsCalculationService transactionsCalculationService;
+
+	public TransactionReceiver(TransactionsCalculationService transactionsCalculationService) {
+		this.transactionsCalculationService = transactionsCalculationService;
+	}
 
 	@RabbitListener(queues = TransactionConstants.ROBO_TRANSACTION_QUEUE_NAME)
 	public void receiveTransaction(String transaction) {
@@ -23,6 +32,7 @@ public class TransactionReceiver {
 			TraderBotTransaction transaction = mapper.readValue(transactionString, TraderBotTransaction.class);
 			log.info(" ****** TRANSACTION RECEIVED: ***** ");
 			log.info(transaction.toString());
+			this.transactionsCalculationService.distributeTransaction(transaction);
 		} catch (Exception e) {
 			log.info(" --- Error during processing JSON value from Rabbit --- ");
 			e.printStackTrace();
